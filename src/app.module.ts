73f8ea394aebe2bd7b91a9f02ebe18f093dbf9b5
zicozydailva <zicozydasilva70@gmail.com>
2024-05-30 09:04:10 +0100

    chore: sync

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
+import { User } from './modules/user/entities/user.entity';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Ensures ConfigModule is available globally
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User],
+        synchronize: true, // Be careful with this option in production
+      }),
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as morgan from 'morgan';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './filters';
+import { LoggerInterceptor } from './interceptors/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.use(cors({}));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan('dev'));
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggerInterceptor());
+
+  const KAFKA_BROKER = configService.get<string>('KAFKA_BROKER');
+
+  const microservice = app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [KAFKA_BROKER],
+      },
+      consumer: {
+        groupId: 'AGENT_WALLET_MS',
+      },
+    },
+  });
+
+  // global config
+  app.setGlobalPrefix('/api');
+  await app.listen(configService.get<string>('PORT'));
+
+  await microservice.listen();
+}
+bootstrap();
